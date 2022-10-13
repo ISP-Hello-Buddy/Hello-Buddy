8094ba754ceeb4c3b464c5530b15e281c133715e
Setthanan Thongpanchang <87871212+reviseUC73@users.noreply.github.com>
2022-10-13 19:11:24 +0700

    update page register add email verify

--- authorize/forms.py
+++ authorize/forms.py
@@ -1,4 +1,4 @@
 from django.contrib.auth.forms import UserCreationForm
 class RegisterForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
-        fields = UserCreationForm.Meta.fields
+        fields = UserCreationForm.Meta.fields + ("email",)
