112f01db7eb61c2a461b143eac752a0ad22097cb
Setthanan Thongpanchang <87871212+reviseUC73@users.noreply.github.com>
2022-10-20 16:19:55 +0700

    #18 ; improve UI login

--- authorize/forms.py
+++ authorize/forms.py
@@ -1,6 +1,5 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django.forms import PasswordInput
 from django import forms
 class RegisterForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
