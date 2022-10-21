0b5987705521aaf886125f7cb19dc27e792cc410
preme <ratthicha.p@ku.th>
2022-10-21 16:43:49 +0700

    connect user database to event

--- Hello_Buddy/views.py
+++ Hello_Buddy/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, redirect
 from django.urls import reverse
 from .forms import CreateEventForm
 from .models import Event
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 # Create your views here.
 
@@ -13,13 +16,16 @@ def create(request):
     if request.method == 'POST':
         form = CreateEventForm(request.POST)
         if form.is_valid():
+            data = form.cleaned_data
             form.save()
+            user = request.user
+            test = Event.objects.filter(name=data['name'])
+            user.event_set.set(test)
             return redirect('home')
     else:
         form = CreateEventForm()
-    context = {'form' : form}
+    context = {'form': form}
     return render(request, 'Hello_Buddy/create_event.html', context)
-
 
 def aboutus(request):
     return render(request, 'Hello_Buddy/aboutus.html')
--- authorize/forms.py
+++ authorize/forms.py
@@ -1,10 +1,13 @@
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 from django import forms
 class RegisterForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = User
         fields = UserCreationForm.Meta.fields + ("email",)
+
         # widgets = {
         #     'username': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Username'}),
         #     'email': forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Email'}),
