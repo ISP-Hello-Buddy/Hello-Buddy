8ff7d6186467034ae0240205e11c3fc9287b6273
breezjirasak <jirasak.tu@ku.th>
2022-11-09 00:00:51 +0700

    fix bug in urls.py

--- Hello_Buddy/urls.py
+++ Hello_Buddy/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from . import views
-from Hello_Buddy.views import event, reverse_to_home
+
 urlpatterns = [
-    path('', reverse_to_home),
+    path('', views.reverse_to_home),
     path('home',views.home, name='home'),
     path('aboutus', views.aboutus, name='aboutus'),
     path('profile-user', views.profile_user, name='profile-user'),
