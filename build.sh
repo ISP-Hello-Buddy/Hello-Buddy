0560e3609687506871141c537b71023469080082
breezjirasak <jirasak.tu@ku.th>
2022-11-22 15:04:22 +0700

    prepare to deploy

--- build.sh
+++ build.sh
@@ -7,4 +7,5 @@ pip install 'whitenoise[brotli]'
 
 python manage.py collectstatic --no-input
 python manage.py migrate
-echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@mail.com', 'admin123456')" | python manage.py shell
+python manage.py createsuperuser
+--no-input
