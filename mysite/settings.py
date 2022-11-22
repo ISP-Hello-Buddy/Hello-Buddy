7f9e78e96522c201357f62cc4d11b7fb39aed57f
breezjirasak <jirasak.tu@ku.th>
2022-11-22 13:59:45 +0700

    prepare to deploy

--- mysite/settings.py
+++ mysite/settings.py
@@ -133,14 +133,7 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 
-if not DEBUG:
-    # Tell Django to copy statics to the `staticfiles` directory
-    # in your application directory on Render.
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-    # Turn on WhiteNoise storage backend that takes care of compressing static files
-    # and creating unique names for each version so they can safely be cached forever.
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = "/static"
 
 # Path where media is stored
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
