70de812628dcd1daf3c07e72f25af5f746e1fc75
breezjirasak <jirasak.tu@ku.th>
2022-11-22 23:47:36 +0700

    prepare to heroku

--- mysite/settings.py
+++ mysite/settings.py
@@ -147,7 +147,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = "/static"
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
