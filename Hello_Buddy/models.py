276dfa4ecd47b275e3e479ed770e772b644e6438
breezjirasak <jirasak.tu@ku.th>
2022-11-23 19:57:02 +0700

    fix event

--- Hello_Buddy/models.py
+++ Hello_Buddy/models.py
@@ -85,14 +85,14 @@ class Profile(models.Model):
     bio = models.TextField(default='...', max_length=50)
 
     # resizing images
-    def save(self, *args, **kwargs):
-        super(Profile, self).save(*args, **kwargs)
-        img = Image.open(self.avatar.path)
+    # def save(self, *args, **kwargs):
+    #     super(Profile, self).save(*args, **kwargs)
+    #     img = Image.open(self.avatar.path)
 
-        if img.height > 100 or img.width > 100:
-            new_img = (100, 100)
-            img.thumbnail(new_img)
-            img.save(self.avatar.path)
+    #     if img.height > 100 or img.width > 100:
+    #         new_img = (100, 100)
+    #         img.thumbnail(new_img)
+    #         img.save(self.avatar.path)
 
     def __str__(self):
         return self.user.username
