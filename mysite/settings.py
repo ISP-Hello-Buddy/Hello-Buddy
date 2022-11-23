93da786a9346b945a6ce9c36ae9bcd8a75be1a21
breezjirasak <jirasak.tu@ku.th>
2022-11-23 10:21:21 +0700

    prepare deploy

--- mysite/settings.py
+++ mysite/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY', default='missing-secret-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False)
 
 ALLOWED_HOSTS = []
 
