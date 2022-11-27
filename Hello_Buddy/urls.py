32a4e03afc8c0f18bff6b79d5c4d100ab31d8761
breezjirasak <jirasak.tu@ku.th>
2022-11-27 16:29:34 +0700

    assign name of  url

--- Hello_Buddy/urls.py
+++ Hello_Buddy/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.reverse_to_home),
+    path('', views.reverse_to_home, name=''),
     path('home', views.home, name='home'),
     path('aboutus', views.about_us, name='aboutus'),
     path('map', views.map, name='map'),
@@ -12,6 +12,5 @@ urlpatterns = [
     path('create', views.create, name='create'),
     path('event/<int:event_id>', views.event, name='event'),
     path('<str:event_category>', views.events_by_category, name='event_category'),
-    # path("test",views.reverse_to_home)
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
